9e18ba43136efa56691a12c6559041ab8854e13f
yuzhuzhong <1310648231@qq.com>
2014-10-27 01:06:35 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,26 +17,49 @@
 #     (do not cut and paste a dataset directly into the text box, as this may cause errors 
 #     saving your submission).
 
+#    read activity_labels.txt
 activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+#    read features.txt
 features <- read.table("./data/UCI HAR Dataset/features.txt")
+#   c function
 datacolnames <- c(as.character(features$V2),"activity_labels","subject")
+#    read subject_train.txt
 subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+#    read train/X_train.txt
 X_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+#    read train/y_train.txt
 y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+#    data.frame function
 training <- data.frame(X_train,y_train$V1,subject_train$V1)
+#    naming
 colnames(training) <- datacolnames 
+#    read subject_test.txt
 subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+#    read X_test.txt
 X_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+#    read y_test.txt
 y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+#    data.frame function
 test <- data.frame(X_test,y_test$V1,subject_test$V1)
+#    naming
 colnames(test) <- datacolnames 
+#    rbind function
 mergedata <- rbind(training,test)
+#    naming
 colnames(mergedata) <- datacolnames
+#    cbind function
 extractsdata <- cbind(mergedata[,grepl("mean()",datacolnames) | grepl("std()",datacolnames)],mergedata[,"activity_labels"],mergedata[,"subject"])
+#    c function
 newnames <- c(datacolnames[grepl("mean()",datacolnames) | grepl("std()",datacolnames)],"activity_labels","subject")
+#    naming
 colnames(extractsdata) <- newnames
+
 extractsdata$activity_labels <- activity_labels$V2[extractsdata$activity_labels]
+#    naming
 colnames(extractsdata) <- newnames
+#    solve 
 second <- aggregate(extractsdata[,1:(ncol(extractsdata)-2)],by=list(extractsdata$activity_labels,extractsdata$subject),FUN = mean)
+#     c
 colnames(second)[1:2] <- c("activity_labels","subject")
+#    write
 write.table(second,"average.txt",row.name=FALSE)
