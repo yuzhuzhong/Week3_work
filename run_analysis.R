fbaaf34e12178e7af2927cd9680ee44b44efa6ff
yuzhuzhong <1310648231@qq.com>
2014-10-27 00:38:01 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,22 @@
+# run_analysis.R should do the following:
+#   
+#   1)  Merges the training and the test sets to create one data set.
+# 
+#   2)  Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 
+#   3)  Uses descriptive activity names to name the activities in the data set
+# 
+#   4)  Appropriately labels the data set with descriptive variable names. 
+#   
+#   5)  From the data set in step 4, creates a second, independent tidy data set with the average
+#       of each variable for each activity and each subject.
+
+#     Please upload the tidy data set created in step 5 of the instructions. 
+
+#     Please upload your data set as a txt file created with write.table() using row.name=FALSE 
+#     (do not cut and paste a dataset directly into the text box, as this may cause errors 
+#     saving your submission).
+
 activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 features <- read.table("./data/UCI HAR Dataset/features.txt")
 datacolnames <- c(as.character(features$V2),"activity_labels","subject")
